542c35557d71d029935ae143bab7280c5f58b53f
Shoichi Kaji <skaji@cpan.org>
2018-03-17 13:36:19 +0900

    -Duseithreads

--- build.sh
+++ build.sh
@@ -16,18 +16,16 @@ perl build.pl 5.22.4
 perl build.pl 5.24.3
 perl build.pl 5.26.1
 
-THREAD_ON=1
-
-perl build.pl 5.8.1  $THREAD_ON
-perl build.pl 5.8.5  $THREAD_ON
-perl build.pl 5.8.8  $THREAD_ON
-perl build.pl 5.8.9  $THREAD_ON
-perl build.pl 5.10.1 $THREAD_ON
-perl build.pl 5.12.5 $THREAD_ON
-perl build.pl 5.14.4 $THREAD_ON
-perl build.pl 5.16.3 $THREAD_ON
-perl build.pl 5.18.4 $THREAD_ON
-perl build.pl 5.20.3 $THREAD_ON
-perl build.pl 5.22.4 $THREAD_ON
-perl build.pl 5.24.3 $THREAD_ON
-perl build.pl 5.26.1 $THREAD_ON
+perl build.pl 5.8.1  -Duseithreads
+perl build.pl 5.8.5  -Duseithreads
+perl build.pl 5.8.8  -Duseithreads
+perl build.pl 5.8.9  -Duseithreads
+perl build.pl 5.10.1 -Duseithreads
+perl build.pl 5.12.5 -Duseithreads
+perl build.pl 5.14.4 -Duseithreads
+perl build.pl 5.16.3 -Duseithreads
+perl build.pl 5.18.4 -Duseithreads
+perl build.pl 5.20.3 -Duseithreads
+perl build.pl 5.22.4 -Duseithreads
+perl build.pl 5.24.3 -Duseithreads
+perl build.pl 5.26.1 -Duseithreads
